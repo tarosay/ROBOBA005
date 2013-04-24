c74222fe736edf4857b0781f0ee2623276653279
Tarosa <tarosa.y@gmail.com>
2013-04-25 04:18:29 +0900

    2013.04.25

--- gr_sketch.cpp
+++ gr_sketch.cpp
@@ -1,9 +1,12 @@
 #include <rxduino.h>
 
-bool ChkDisconnectFlag = false;
-int CntPDisconnected = 0;
-static unsigned char DISCONNECT[] = "+DISCONNECTED=";
-unsigned char Cdat;
+#define DISCONNECTED_CNT 14
+static unsigned char DISCONNECTED[] = "+DISCONNECTED=";
+
+bool CommandMode = true;
+bool ChkConnectFlag = false;
+int CntConnect = 0;
+unsigned char Ct;
 
 //**************************************************
 // ROBOBA005のセットアップ
@@ -11,12 +14,17 @@ unsigned char Cdat;
 //**************************************************
 void ROBOBA005_Setup( void )
 {
-    Serial1.begin(115200, SCI_SCI2B);
-    pinMode(PIN_P51 ,OUTPUT);
+	//XBee用にSerial1をオープン
+	Serial1.begin(115200, SCI_SCI2B);
 
-    digitalWrite( PIN_P51, 0 );
+	pinMode(PIN_P51 ,OUTPUT);
+
+	//XBeeをリセット
+	digitalWrite( PIN_P51, 0 );
     delay(10);
-    digitalWrite( PIN_P51, 1 );
+
+	//XBeeをリセット解除
+	digitalWrite( PIN_P51, 1 );
     delay(10);
 }
 
@@ -25,28 +33,28 @@ void ROBOBA005_Setup( void )
 //**************************************************
 void setup()
 {
-    pinMode( PIN_LED0, OUTPUT);
-    pinMode( PIN_LED1, OUTPUT);
-    pinMode( PIN_LED2, OUTPUT);
-    pinMode( PIN_LED3, OUTPUT);
+	pinMode( PIN_LED0, OUTPUT);
+	pinMode( PIN_LED1, OUTPUT);
+	pinMode( PIN_LED2, OUTPUT);
+	pinMode( PIN_LED3, OUTPUT);
 
-    Serial.begin(115200);
-    //Serial.begin( 115200, SCI_AUTO );    	  //何か押されるまで待つ
-    Serial.setDefault();                	//現在のシリアルポートを標準出力とするprintfでも出力する
-    setvbuf(stdout,NULL,_IONBF,0);
+	Serial.begin(115200);
+    //Serial.begin( 115200, SCI_AUTO );	//何か押されるまで待つ
+	Serial.setDefault();				//現在のシリアルポートを標準出力とするprintfでも出力する
+	setvbuf(stdout,NULL,_IONBF,0);
 
-    //ROBOBA005セットアップ
-    ROBOBA005_Setup();
+	//ROBOBA005セットアップ
+	ROBOBA005_Setup();
 
-    //基板の青ボタンを有効にします
-    pinMode(PIN_SW, INPUT);
+	//基板の青ボタンを有効にします
+	pinMode(PIN_SW, INPUT);
 
-    //接続待ちモードにします
-    Serial1.print("AT+RESET\r\n");
-    delay(1000);
-    Serial1.print("AT+CLRPAIR\r\n");
-    delay(1000);
-    Serial1.print("AT+CONSLAVE2=1,1\r\n");
+	//接続待ちモードにします
+	Serial1.print("AT+RESET\r\n");
+	delay(1000);
+	Serial1.print("AT+CLRPAIR\r\n");
+	delay(1000);
+	Serial1.print("AT+CONSLAVE2=1,1\r\n");
 }
 
 //**************************************************
@@ -54,53 +62,51 @@ void setup()
 //**************************************************
 void loop()
 {
-    while(Serial.available()) //何か受信があった
-    {
-        Cdat = (unsigned char)Serial.read(); //1文字取得
-        Serial1.write( Cdat );
-    }
+	while(Serial.available()) //何か受信があった
+	{
+		Ct = (unsigned char)Serial.read(); //1文字取得
+		Serial1.write( Ct );
+	}
 
-    while(Serial1.available()) //何か受信があった
-    {
-        Cdat = (unsigned char)Serial1.read(); //1文字取得
+	while(Serial1.available()) //何か受信があった
+	{
+		Ct = (unsigned char)Serial1.read(); //1文字取得
 
-        if( Cdat=='+' && ChkDisconnectFlag==false ){
+		if( Ct=='+' && ChkConnectFlag==false ){
 
-            ChkDisconnectFlag = true;
-            CntPDisconnected++;
+			ChkConnectFlag = true;
+			CntConnect++;
 
-        }
-        else if( ChkDisconnectFlag==true && Cdat==DISCONNECT[CntPDisconnected] ){
+		}
+		else if( ChkConnectFlag==true && Ct==DISCONNECTED[CntConnect] ){
+			CntConnect++;
+			if( CntConnect==DISCONNECTED_CNT ){
 
-            CntPDisconnected++;
-            if( CntPDisconnected==14 ){
+				//+DISCONNECTED確定！
+				//接続待ちモードにします
+				Serial1.print("AT+RESET\r\n");
+				delay(1000);
+				Serial1.print("AT+CLRPAIR\r\n");
+				delay(1000);
+				Serial1.print("AT+CONSLAVE2=1,1\r\n");
 
-                //+DISCONNECTED確定！接続待ちモードにします
-                Serial1.print("AT+RESET\r\n");
-                delay(1000);
-                Serial1.print("AT+CLRPAIR\r\n");
-                delay(1000);
-                Serial1.print("AT+CONSLAVE2=1,1\r\n");
+				CntConnect = 0;
+				ChkConnectFlag = false;
+			}
+		}
+		else{
+			CntConnect = 0;
+			ChkConnectFlag = false;
+		}
+		Serial.write( Ct );
+	}
 
-                CntPDisconnected = 0;
-                ChkDisconnectFlag = false;
-            }
-        }
-        else{
-            CntPDisconnected = 0;
-            ChkDisconnectFlag = false;
-        }        
-        
-        Serial.write( Cdat );
-    }
+	//基板の青ボタンを押して接続を切ります
+	if( digitalRead(PIN_SW)==LOW ){
+		
+		//青ボタンが離されるまで待つ
+		while(digitalRead(PIN_SW)==LOW);
 
-    if( digitalRead(PIN_SW)==LOW ){
-        
-        //青ボタンが離されるまで待つ
-        while(digitalRead(PIN_SW)==LOW);
-
-        //通信を切断するために+++を送信
-        Serial1.print( "+++" );
-    }
+		Serial1.print( "+++" );
+	}
 }
-
